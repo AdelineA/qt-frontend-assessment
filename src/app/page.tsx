ec16f1056d52fadf345725db4606a21d026c1b1e
AdelineA <adelinemug6@gmail.com>
2025-03-10 11:37:35 +0200

    feat: project init

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-amber-300 text-5xl text-center font-bold h-screen flex items-center justify-center">
+      Welcome To My App
+    </div>
+  );
+}
